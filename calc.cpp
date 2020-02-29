ae9654921caa58064fdab0ca14c2d6a821d99170
rajbhowmick27 <rajbhowmick27@gmail.com>
2020-02-29 14:40:30 +0530

    Calc.cpp file added

---
+++ calc.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace tsd;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<"\nAdd 2 nos . "<<a+b;
+    cout<<"\nSubtract 2 nos."<<abs(a-b);
+    cout<<"\nMultiply 2 nos. "<<a*b;
+    cout<<"\nDivide 2 nos."<<a/b;
+    return 0;
+}
