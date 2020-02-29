25a35bdf19bdf8ddba94feb3ad72d10e436beddf
rajbhowmick27 <rajbhowmick27@gmail.com>
2020-02-29 15:37:18 +0530

    Updated 0's pos

--- calc.cpp
+++ calc.cpp
@@ -5,7 +5,11 @@ int main()
 {
     int a,b;
     cin>>a>>b;
-    cout<<"\nAdd 2 nos . "<<a+b;
+<<<<<<< Updated upstream
+    cout<<"\nAdd 2 nos . "<<(a+b);
+=======
+    cout<<"\nAdd 2 nos . "<<a+0+b;
+>>>>>>> Stashed changes
     cout<<"\nSubtract 2 nos."<<abs(a-b);
     cout<<"\nMultiply 2 nos. "<<a*b;
     cout<<"\nDivide 2 nos."<<b/a;
